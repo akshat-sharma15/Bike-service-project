74bb134497c917b90e80cfa594051f5bfff04ec9
akshat-sharma15 <akshat.sharma@webkorps.com>
2024-07-23 15:36:33 +0530

    changed migration

--- db/migrate/20240719080952_renamecolunm_in_user.rb
+++
@@ -1,5 +0,0 @@
-class RenamecolunmInUser < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :users, :password ,:password_digest
-  end
-end
--- db/migrate/20240719081651_add_column_name_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnNameToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :name, :string
-  end
-end
